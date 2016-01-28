72c456ab2874f72de6a7131f59fc9b79980208a9
a3group <info@armisoft.net>
2016-01-29 01:17:17 +0400

    addition fragments of dispatcher of tasks

--- src/tvision.cpp
+++ src/tvision.cpp
@@ -12,26 +12,69 @@
 
 //Number of processor's kernels (need to effictively load processor by creating number of threads)
 const int nKernels = 4;
+//Number of tasks (not need to be equal number of kernel)
+const int nTasks = 4;
 
 using namespace std;
 
+enum taskState{
+	free = 0,
+	busy,
+	need
+};
+
+struct staskPri{
+	CTaskProcess *pCTaskProcess;
+	taskState state;
+}
+
 int main(int argc, char **argv)
 {
+	CVCapProcess VCapProcess;
 	CVideoProcess PointsProcess;
 	CVoiceProcess VoiceProcess;
-	CVCapProcess VCapProcess;
 	CNetProcess NetProcess;
+
+	vector<staskPri> tasksPri;
+	tasksPri.resize(nTasks);
+	tasksPri[0].pCTaskProcess = &VCapProcess; tasksPri[0].state = need;
+	tasksPri[1].pCTaskProcess = &CVideoProcess; tasksPri[1].state = need;
+	tasksPri[2].pCTaskProcess = &CVoiceProcess; tasksPri[2].state = need;
+	tasksPri[3].pCTaskProcess = &CNetProcess; tasksPri[3].state = need;
 
 	// create thread pool with 4 worker threads
 	ThreadPool poold(nKernels);
 
-	auto r1 = poold.runAsync<int>(&CVideoProcess::processRun, &PointsProcess, 5);
+	//creating first load at pools
+	auto r1 = poold.runAsync<int>([](int i){return i});
+	auto r2 = poold.runAsync<int>([](int i){return i});
+	auto r3 = poold.runAsync<int>([](int i){return i});
+	auto r4 = poold.runAsync<int>([](int i){return i});
 
+/*
+	auto r1 = poold.runAsync<int>(&CVideoProcess::processRun, &PointsProcess, 5);
 	auto r2 = poold.runAsync<int>(&CVoiceProcess::processRun, &VoiceProcess, 4);
 	auto r3 = poold.runAsync<int>(&CVCapProcess::processRun, &VCapProcess, 5);
 	auto r4 = poold.runAsync<int>(&CNetProcess::processRun, &NetProcess, 4);
+*/
 
 	vector<int> state = {0,0,0,0};
+
+	//while(1){
+	//	checking pool size
+	//	if(pool.size == FULL){
+	//		sleep(1);
+	//		continue;
+	//	}
+
+	//	for_each(tasksPri.begin, tasksPri.end, [](staskPri &task){
+	//		if(task.state == need){
+	//			//add to pool if free
+
+	//			task.state = need;
+	//		}
+	//	})
+	//}
 
 	//here get tasks from task's queue
 	while(1){
